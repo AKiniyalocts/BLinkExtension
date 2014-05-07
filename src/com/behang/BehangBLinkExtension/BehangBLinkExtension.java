e456f016821ef33e4222ffc115039277b484f8a4
A_Kiniyalocts <kiniyaloca1@gmail.com>
2014-05-07 01:26:12 -0400

    finished example extension

---
+++ src/com/behang/BehangBLinkExtension/BehangBLinkExtension.java
@@ -0,0 +1,11 @@
+package com.behang.BehangBLinkExtension;
+
+/**
+ * Created by anthony on 5/7/14.
+ */
+public class BehangBLinkExtension extends BLinkExtension{
+
+    public BehangBLinkExtension() {
+        super("Behang");
+    }
+}
